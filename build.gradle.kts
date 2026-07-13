d2deb1de3196bd26c00f7af07f121f36238b63a9
movte <movtery228@qq.com>
2026-07-13 16:42:29 +0800

    feat: push to jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
 }
 
 android {
@@ -42,4 +43,44 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = project.findProperty("GROUP") as String? ?: "com.github.ZalithLauncher"
+                artifactId = project.findProperty("POM_ARTIFACT_ID") as String? ?: rootProject.name
+                version = project.findProperty("VERSION_NAME") as String? ?: "unspecified"
+
+                pom {
+                    name.set(project.findProperty("POM_NAME") as String? ?: rootProject.name)
+                    description.set(project.findProperty("POM_DESCRIPTION") as String? ?: "")
+                    url.set(project.findProperty("POM_URL") as String? ?: "")
+
+                    licenses {
+                        license {
+                            name.set(project.findProperty("POM_LICENCE_NAME") as String? ?: "")
+                            url.set(project.findProperty("POM_LICENCE_URL") as String? ?: "")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("BZLZHH")
+                            name.set("BZLZHH")
+                        }
+                    }
+
+                    scm {
+                        url.set(project.findProperty("POM_SCM_URL") as String? ?: "")
+                        connection.set(project.findProperty("POM_SCM_CONNECTION") as String? ?: "")
+                        developerConnection.set(project.findProperty("POM_SCM_DEV_CONNECTION") as String? ?: "")
+                    }
+                }
+            }
+        }
+    }
 }
